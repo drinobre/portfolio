3337cf8a244721438c9a303314c483e2ba12bbae
drinobre <dri_nobre@hotmail.com>
2022-02-11 14:31:50 +0000

    fix deployment issues

--- portifolio/pages/index.js
+++ portifolio/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Navbar } from "../components/Navbar";
 
@@ -8,33 +7,9 @@ export default function Home() {
     <div className={styles.container}>
       <Navbar></Navbar>
       <Head>
-        <title>Hi, I'm Adriana</title>
+        <title>Hi, I am Adriana</title>
         <meta name="description" content="Adriana Nobre Lawrence portifolio" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-
-      {/* <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-        </div>
-      </main> */}
 
       <footer className={styles.footer}>
         <a
