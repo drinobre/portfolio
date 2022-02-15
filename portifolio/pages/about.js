46ae8a8bce5f1e323953fb011782e7eacfdf83c8
drinobre <dri_nobre@hotmail.com>
2022-02-15 20:37:13 +0000

    update project and babel

--- portifolio/pages/about.js
+++ portifolio/pages/about.js
@@ -1,11 +1,9 @@
 import { Navbar } from "../components/Navbar";
-import Image from "next/image";
 
 export default function About() {
   return (
     <>
       <Navbar></Navbar>
-      <body></body>
     </>
   );
 }
