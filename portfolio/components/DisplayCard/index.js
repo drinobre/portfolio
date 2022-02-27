26aef775e470bf8ed65f60b4173c1a088cfb8580
drinobre <dri_nobre@hotmail.com>
2022-02-27 15:29:12 +0000

    change the project name to Portfolio

---
+++ portfolio/components/DisplayCard/index.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+export default function DisplayProjects({
+  title,
+  src,
+  alt,
+  fullDescription,
+  technology,
+  type,
+  gitHubLink,
+  link,
+}) {
+  return (
+    <section className="section-group">
+      <div className="project-results1">
+        <div className="container-project"></div>
+        <div className="project-group1">
+          <div className="project-group-image">
+            <Image
+              src="/images/personal_avatar.png"
+              alt="Adriana Nobre Lawrence Avatar"
+              width="10"
+              height="10"
+              layout="responsive"
+            />
+            <h4 className="subheader-gray">{type}</h4>
+          </div>
+          <div className="project-group-text">
+            <div>
+              <h2>{title}</h2>
+              <h3 className="subheader-gray">{technology}</h3>
+            </div>
+            <p className="title">{fullDescription}</p>
+            <br></br>
+            <div>
+              <a href={link}>See project</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ portfolio/pages/index.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Navbar } from "../components/Navbar";
+import PagesHeader from "../components/Header";
+import HeroHomePage from "../components/Hero";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Navbar></Navbar>
+      <Head>
+        <title>Hi, I am Adriana</title>
+        <meta name="description" content="Adriana Nobre Lawrence portifolio" />
+      </Head>
+      <PagesHeader
+        title={"Hello, I'm Adriana Lawrence"}
+        subheader={"< Mum, Entrepreneur, Full Stack Developer >"}
+        // text={
+        //   "Starting a new career in Tech can be very challenging and I've taken the hardest step of leaving a promising career as a Business Analyst | Project Manager to become a Full Stack Developer. The rest of the story will depend on who is willing to help me grow? I hope you enjoy learning more about me and see a bit more about what I have done so far ..."
+        // }
+      ></PagesHeader>
+
+      <HeroHomePage></HeroHomePage>
+
+      <Footer></Footer>
+    </div>
+  );
+}
